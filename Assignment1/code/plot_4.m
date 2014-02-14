071d68833cc1d7a366e85bd57f2e8137d84d587e
sudhargk <sudharpun90@gmail.com>
2014-02-14 18:51:52 +0530

    Completed Plot 4

--- Assignment1/code/plot_4.m
+++ Assignment1/code/plot_4.m
@@ -6,7 +6,7 @@
 % lambda : regularization parameter
 % E.g 
 %       plot_4('bivariate','Guassian',10,exp(-10),2);
-%       plot_5('univariate','Polynomial',10,exp(-10),1);
+%       plot_4('univariate','Polynomial',10,exp(-10),1);
 
 function [] =plot_4(data,basisType,basis,lambda,variance)
     [trainX,trainT] = importd(data,'train'); 
@@ -17,23 +17,30 @@ function [] =plot_4(data,basisType,basis,lambda,variance)
     testXPhi = computePhiFromDesign(testX,basisType,basis,variance,M);
     valXPhi = computePhiFromDesign(valX,basisType,basis,variance,M);
     W = train(trainXPhi,trainT,lambda);
-    hold on;
-    figure(1);
-    X = trainX(:,1); Y = trainX(:,2); Z = 0;
-    plot3(trainT,trainXPhi*W,'ro');
-    scatter(testT,testXPhi*W,'g+');
-    scatter(valT,valXPhi*W,'b*');
-    axis equal;
-    legend('train','test','validation');
-    T = [ trainT;testT;valT];
-    axis([min(T) max(T) min(T) max(T)]);
+    generatePlot(trainX,trainXPhi*W,trainT,data,'train');
+    generatePlot(testX,testXPhi*W,testT,data,'test');
+    generatePlot(valX,valXPhi*W,valT,data,'validation');
     
-    grid on;
-    hold off; 
 end
 
-function [] = generatePlot(input, output, target, modelType)
-    switch modelType
-      case 'univariate':
+function [] = generatePlot(input, output, target, data,dataType)
+    figure();
+    switch (data);
+        case 'univariate' 
+                    X = input(:,1); Y2 = output(:,1); Y1 = target(:,1); 
+                    Z = zeros(size(input,1),1);
+        case 'bivariate'
+                    X = input(:,1); Y2 = output(:,1); Y1 = target(:,1);
+                    Z = input(:,2);
     end
+    hold on;
+    title(['Plot of Target output and Model output on ' dataType ' data for ' data ' dataset'])
+    
+    plot3(X,Y1,Z,'g+');
+    plot3(X,Y2,Z,'ro');
+    legend('Target Output','Model Output');
+    xlabel('Dimension 1');
+    ylabel('Output');
+    zlabel('Dimension 2');
+    hold off;
 end
