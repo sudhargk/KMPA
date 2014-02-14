d43ef025644b6930ee7823b6b7d26fa2156538f7
sudhargk <sudharpun90@gmail.com>
2014-02-14 19:31:43 +0530

    Updated plot to accomodate subplot

--- Assignment1/code/plot_1.m
+++ Assignment1/code/plot_1.m
@@ -1,13 +1,21 @@
-
-function [] =plot_1()
-    [X,T] = import('univariate','train'); 
-    n = [50];
-    for i = 1:length(n)
-        idx = randperm(size(X,1),n(i));
+%%Plot of Actual function,Target output and Model output for Dataset 1 for different dataset size different model complexity
+%% and different regularization parameters
+% N  : 1 X N column vector for different dataset size
+% B :  1 X N column vector for different basis values
+% L : 1 X N column vector for different lambda values
+% Remarks :: higher regularization needed for higher basis 
+% plot_1([10 20],[4 20],exp([-25 -20]))
+function [] = plot_1(N,B,L)
+    [X,T] = importd('univariate','train'); 
+    for i = 1:length(N)
+        figure();
+        idx = randperm(size(X,1),N(i));
         newX = X(idx,:);newT = T(idx);
-        for basis = [50]
+        plotIdx =1;
+        for basis = B
             xPhi = computeDesignMatrix(newX,'Polynomial',basis);
-            for lambda = exp([-Inf -20 0])
+            for lambda = L
+                subplot(length(B),length(L),plotIdx);
                 W= train(xPhi,newT,lambda);
                 %Plotting 
                 Z = linspace(0,1,1000)';
@@ -15,17 +23,13 @@ function [] =plot_1()
                 zY  = zPhi*W;
                 zF = f(Z);
                 plot(Z,zF,'g',Z,zY,'r',newX,newT,'o');
+                title(['d =' num2str(basis) '  ln(\lambda) = ' num2str(log(lambda))]);
                 ylim([-6 2]);
+                plotIdx=plotIdx +1;
             end
         end
         
         
     end
     
-end
-function [X,T] = import(type, filetype)
-    t = [pwd '\..\data\' type '\' filetype '.txt'];
-    Z = importdata(t);
-    X = Z(:,1:size(Z,2)-1);
-    T = Z(:,size(Z,2));
 end
